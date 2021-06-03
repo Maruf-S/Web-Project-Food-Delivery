4163cd9a96acda188ff1c5f38cd2d6539f34f6dc
Taklemariam Alazar <teklealazar399@gmail.com>
2021-06-03 03:16:22 +0300

    updates on the index page

--- Food_Delivery/Controllers/HomeController.cs
+++ Food_Delivery/Controllers/HomeController.cs
@@ -6,20 +6,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Food_Delivery.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Food_Delivery.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
         [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Fres = _context.Restaurants.ToList();
             return View();
         }
         public IActionResult About() {
@@ -29,6 +38,17 @@ namespace Food_Delivery.Controllers
         {
             return View();
         }
+
+        /*[HttpGet]
+        public IActionResult RestourantDetail(string query)
+        {
+            if (query == null)
+            {
+                return View(_context.Restaurants.ToList());
+            }
+            ViewBag.query = query;
+            return View(_context.Restaurants.Where(r => r.Name.StartsWith(query)).ToList());
+        }*/
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
